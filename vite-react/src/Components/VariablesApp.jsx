8688eaeb41aa729a15fbe0eb286e2d18aa668c7e
Scana2212 <aguscanavino@gmail.com>
2024-07-03 16:21:05 -0300

    Cambios estilos

---
+++ vite-react/src/Components/VariablesApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const VariablesApp = () => {
+  return (
+    <>
+        <h1>Texto de estilos</h1>
+        <h2 id="subtitulo">Utilizando distintos selectores</h2>
+        <h4 className="red">Titulo rojo</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel dolorem corporis unde eos minima maiores quam enim est accusantium iste quis molestias, voluptas qui maxime pariatur eveniet recusandae placeat saepe?</p>
+    </>
+    
+  )
+}
